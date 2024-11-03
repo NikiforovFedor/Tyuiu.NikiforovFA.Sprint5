5c80f56c4e56dd44d2e464abba51f09d6cafcc00
Fedor Nikiforov <Tyuiu.NikiforovFA@outlook.com>
2024-11-03 18:11:59 +0500

    Task0

--- Tyuiu.NikiforovFA.Sprint5.Task0.V19.Lib/DataService.cs
+++ Tyuiu.NikiforovFA.Sprint5.Task0.V19.Lib/DataService.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.NikiforovFA.Sprint5.Task0.V19.Lib
         public string SaveToFileTextData(int x)
         {
             double r = Math.Round((2 * x * x - 1) / (Math.Sqrt(x * x - 2)), 3);
-            string s = Path.Combine($@"{Directory.GetCurrentDirectory()}", $@"\OnePutFileTask0.txt");
+            string s = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask0.txt" });
             File.WriteAllText(s, Convert.ToString(r));
             return s;
         }
