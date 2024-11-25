c2662ebcd7963947f3ad998403a16bc8f12fb803
Fedor Nikiforov <Tyuiu.NikiforovFA@outlook.com>
2024-11-25 16:50:03 +0500

    Task2

--- Tyuiu.NikiforovFA.Sprint5.Task2.V3.Lib/DataService.cs
+++ Tyuiu.NikiforovFA.Sprint5.Task2.V3.Lib/DataService.cs
@@ -7,6 +7,8 @@ namespace Tyuiu.NikiforovFA.Sprint5.Task2.V3.Lib
         public string SaveToFileTextData(int[,] matrix)
         {
             double res;
+            int rows = matrix.GetLength(0) + 1;
+            int colomn = matrix.Length / rows;
             File.Delete("OutPutFileTask2.txt");
             string s = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask2.txt" });
             for (int i = 0; i < matrix.GetLength(0); i++)
@@ -20,11 +22,11 @@ namespace Tyuiu.NikiforovFA.Sprint5.Task2.V3.Lib
                 }
             }
             string str = "";
-            for (int i = 0;i < matrix.GetLength(0); i++)
+            for (int i = 0;i < rows; i++)
             {
-                for(int j = 0;j < matrix.GetLength(1); j++)
+                for(int j = 0;j < colomn; j++)
                 {
-                    if (j != matrix.GetLength(1))
+                    if (j != colomn)
                     {
                         str += matrix[i, j] + ";" ;
                     }
@@ -33,7 +35,7 @@ namespace Tyuiu.NikiforovFA.Sprint5.Task2.V3.Lib
                         str += matrix[i,j];
                     }
                 }
-                if (i != matrix.GetLength(0) - 1)
+                if (i != rows - 1)
                 {
                     File.AppendAllText(s, str + Environment.NewLine);
                 }
