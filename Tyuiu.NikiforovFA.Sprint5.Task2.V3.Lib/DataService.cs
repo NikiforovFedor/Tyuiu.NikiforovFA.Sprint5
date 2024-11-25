16244a7ee2a24ede261c4c834c2f04d69a49a384
Fedor Nikiforov <Tyuiu.NikiforovFA@outlook.com>
2024-11-25 16:51:01 +0500

    Task2

--- Tyuiu.NikiforovFA.Sprint5.Task2.V3.Lib/DataService.cs
+++ Tyuiu.NikiforovFA.Sprint5.Task2.V3.Lib/DataService.cs
@@ -11,9 +11,9 @@ namespace Tyuiu.NikiforovFA.Sprint5.Task2.V3.Lib
             int colomn = matrix.Length / rows;
             File.Delete("OutPutFileTask2.txt");
             string s = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask2.txt" });
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int i = 0; i < rows i++)
             {
-                for (int j = 0; j < matrix.GetLength(1); j++)
+                for (int j = 0; j < colomn; j++)
                 {
                     if (matrix[i,j] % 2 != 0)
                     {
@@ -26,7 +26,7 @@ namespace Tyuiu.NikiforovFA.Sprint5.Task2.V3.Lib
             {
                 for(int j = 0;j < colomn; j++)
                 {
-                    if (j != colomn)
+                    if (j != colomn-1)
                     {
                         str += matrix[i, j] + ";" ;
                     }
